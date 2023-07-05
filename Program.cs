481760fe2a8c6866815b9dbe3d82fbb16f98592f
muratBee01 <muratalibek01@gmail.com>
2023-07-05 16:00:13 +0600

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,40 @@
+﻿namespace Leetcode26
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter size of an Array!");
+            int arraySize = Convert.ToInt16(Console.ReadLine());
+            int k = 0;
+            Console.WriteLine("Array size equal to " + arraySize);
+
+            Console.WriteLine("Enter array elements: ");
+            int[] array = new int[arraySize];
+            for (int i = 0; i < arraySize; i++)
+            {
+                array[i] = Convert.ToInt16(Console.ReadLine());
+            }
+            int[] arrayClean = array.Distinct().ToArray();
+            // Manual dublicated array element searching code
+            //-----------------------------------------------
+            //for (int i = 0; i<arraySize; i++)
+            //{
+            //    for(int j = i; j < arraySize; j++)
+            //    {
+            //        if (array[i] == array[j])
+            //        {
+            //            k++;
+            //        }
+            //    }
+            //}
+            k = arrayClean.Length;
+            for (int i = 0; i < arrayClean.Length; i++)
+            {
+                Console.Write(arrayClean[i] + " ");
+            }
+            Console.WriteLine();
+            Console.ReadKey();
+        }
+    }
+}
